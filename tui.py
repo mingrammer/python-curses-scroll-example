6d606338addd131c8198f9fc5ebf26806fb22569
mingrammer <mingrammer@gmail.com>
2018-03-05 02:07:18 +0900

    Create a python curses example

---
+++ tui.py
@@ -0,0 +1,151 @@
+import curses
+import curses.textpad
+
+
+class Screen(object):
+    UP = -1
+    DOWN = 1
+
+    def __init__(self, items):
+        """ Initialize the screen window
+
+        Attributes
+            window: A full curses screen window
+
+            width: The width of `window`
+            height: The height of `window`
+
+            max_lines: Maximum visible line count for `result_window`
+            top: Available top line position for current page (used on scrolling)
+            bottom: Available bottom line position for whole pages (as length of items)
+            current: Current highlighted line number
+            page: Total page count which being changed dynamically corresponding to result of a query
+
+        Returns
+            None
+        """
+        self.window = None
+
+        self.width = 0
+        self.height = 0
+
+        self.init_curses()
+
+        self.items = items
+
+        self.max_lines = curses.LINES
+        self.top = 0
+        self.bottom = len(self.items)
+        self.current = 0
+        self.page = self.bottom // self.max_lines
+
+        self.run()
+
+    def init_curses(self):
+        """Setup the curses"""
+        self.window = curses.initscr()
+        self.window.keypad(True)
+
+        curses.noecho()
+        curses.cbreak()
+
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_CYAN)
+
+        self.current = curses.color_pair(2)
+
+        self.height, self.width = self.window.getmaxyx()
+
+    def run(self):
+        """Continue running the TUI until get interrupted"""
+        try:
+            self.input_stream()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            curses.endwin()
+
+    def input_stream(self):
+        """Waiting an input and run a proper method according to type of input"""
+        while True:
+            self.display()
+
+            ch = self.window.getch()
+            if ch == curses.KEY_UP:
+                self.scroll(self.UP)
+            elif ch == curses.KEY_DOWN:
+                self.scroll(self.DOWN)
+            elif ch == curses.KEY_LEFT:
+                self.paging(self.UP)
+            elif ch == curses.KEY_RIGHT:
+                self.paging(self.DOWN)
+            elif ch == curses.ascii.ESC:
+                break
+
+    def scroll(self, direction):
+        """Scrolling the result window when pressing up/down arrow keys"""
+        # next cursor position after scrolling
+        next_line = self.current + direction
+
+        # Up direction scroll overflow
+        # current is 0, but top is greater than 0 => there is available items above current cursor position
+        if (direction == self.UP) and (self.top > 0 and self.current == 0):
+            self.top += direction
+            return
+        # Down direction scroll overflow
+        # next cursor touch the max line, but is less than bottom => there is available items below next cursor position
+        if (direction == self.DOWN) and (next_line == self.max_lines) and (self.top + self.max_lines < self.bottom):
+            self.top += direction
+            return
+        # Scroll up
+        # current or top is greater than 0, so there are more items. we can scroll up
+        if (direction == self.UP) and (self.top > 0 or self.current > 0):
+            self.current = next_line
+            return
+        # Scroll down
+        # next cursor position is less than than max line, so there are more items. we can scroll down
+        if (direction == self.DOWN) and (next_line < self.max_lines) and (self.top + next_line < self.bottom):
+            self.current = next_line
+            return
+
+    def paging(self, direction):
+        """Paging the result window when pressing left/right arrow keys"""
+        current_page = (self.top + self.current) // self.max_lines
+        next_page = current_page + direction
+        # The last page may have items less than max line,
+        # so we should adjust the current cursor position as maximum item count on last page
+        if next_page == self.page:
+            self.current = min(self.current, self.bottom % self.max_lines - 1)
+
+        # Page up
+        # current page is greater than 0, so we can page up
+        # top can not be negative, so if top is going to be negative value,
+        # we should set it as 0 (top position of window)
+        if (direction == self.UP) and (current_page > 0):
+            self.top = max(0, self.top - self.max_lines)
+            return
+        # Page down
+        # current page is less than total page count, so we can page down
+        if (direction == self.DOWN) and (current_page < self.page):
+            self.top += self.max_lines
+            return
+
+    def display(self):
+        """Display the items on window"""
+        self.window.erase()
+        for idx, item in enumerate(self.items[self.top:self.top + self.max_lines]):
+            if idx == self.current:
+                self.window.addstr(idx, 0, item, curses.color_pair(2))
+            else:
+                self.window.addstr(idx, 0, item, curses.color_pair(1))
+        self.window.refresh()
+
+
+def main():
+    items = [f'{num + 1}. Item' for num in range(1000)]
+    Screen(items)
+
+
+if __name__ == '__main__':
+    main()
